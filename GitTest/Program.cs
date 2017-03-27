c1a246ee5c373c394d5d4ee8fbd240776aada5e8
sTexnik <el.glushen@yandex.by>
2017-03-27 22:57:16 +0300

    Update Program.cs

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,5 +20,9 @@ namespace GitTest
         {
             Console.WriteLine("Функция Влада");
         }
+        static void AlexMethod()
+        {
+            Console.WriteLine("Метод Алексея");
+           }
+        }
     }
-}
